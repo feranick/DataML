097baa34c3fa41f01a850abe1a51f4d674a5879a
Nicola Ferralis <feranick@hotmail.com>
2019-06-25 17:43:04 -0400

    Added model converters for TF Lite and TF JS

---
+++ src/utilities/ConvertToTFJS.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+**********************************************************
+* Convert TF models (TF, Keras) into TF.js
+* 20190625a
+* Uses: TensorFlow, Keras
+* By: Nicola Ferralis <feranick@hotmail.com>
+***********************************************************
+'''
+print(__doc__)
+import tensorflow as tf
+import tensorflowjs as tfjs
+import sys, os.path, h5py
+import keras
+#import tensorflow.keras as keras
+
+#************************************
+# Main
+#************************************
+def main():
+    if len(sys.argv) < 2:
+        print(' Usage:\n  python3.py ConvertToTFJS <model in HDF5 format>\n')
+        print(' Requires python 3.x. Not compatible with python 2.x\n')
+        return
+    else:
+        print('\n Converting',sys.argv[1],'to TF.js...\n')
+        convertModelToTFLite(sys.argv[1])
+
+#************************************
+# Convert TF Model to TF.Lite
+#************************************
+def convertModelToTFLite(savedModel):
+    # Remove this wnen transitioning to tensorflow 2.0
+    tf.compat.v1.disable_eager_execution()
+    model = keras.models.load_model(savedModel)
+    convFile = os.path.splitext(savedModel)[0]+'_js'
+    tfjs.converters.save_keras_model(model, convFile)
+    print(' Converted model saved inside:',convFile,'\n')
+
+#************************************
+# Main initialization routine
+#************************************
+if __name__ == "__main__":
+    sys.exit(main())
+
+
