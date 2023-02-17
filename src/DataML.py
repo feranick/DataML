e95729ef86d63b2f81f4463df0b554024b398853
Nicola Ferralis <feranick@hotmail.com>
2023-02-17 16:22:22 -0500

    Removed deprecated optimizers for TF < 2.11

--- src/DataML.py
+++ src/DataML.py
@@ -3,7 +3,7 @@
 '''
 **********************************************************
 * DataML Classifier and Regressor
-* 20221103a
+* 20230217a
 * Uses: TensorFlow
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************************
@@ -311,23 +311,14 @@ def train(learnFile, testFile, normFile):
         #************************************
         ### Define optimizer
         #************************************
-        if checkTFVersion():
-            # New version
-            print("  Using new optimizer for TF > = 2.11\n")
-            lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-                initial_learning_rate=dP.l_rate,
-                decay_steps=dP.epochs,
-                decay_rate=dP.l_rdecay)
-            optim = keras.optimizers.Adam(learning_rate=lr_schedule, beta_1=0.9,
-                    beta_2=0.999, epsilon=1e-08,
-                    amsgrad=False)
-        else:
-        # Legacy optimizer
-            print("  Using legacy optimizer for TF < 2.11\n")
-            optim = keras.optimizers.legacy.Adam(learning_rate=dP.l_rate, beta_1=0.9,
-                    beta_2=0.999, epsilon=1e-08,
-                    decay=dP.l_rdecay,
-                    amsgrad=False)
+        
+        lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=dP.l_rate,
+            decay_steps=dP.epochs,
+            decay_rate=dP.l_rdecay)
+        optim = keras.optimizers.Adam(learning_rate=lr_schedule, beta_1=0.9,
+                beta_2=0.999, epsilon=1e-08,
+                amsgrad=False)
         
         #************************************
         ### Build model
--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=['numpy', 'h5py', 'tensorflow','scipy','pydot'],
     entry_points={'console_scripts' : ['DataML=DataML:DataML','ConvertLabel=ConvertLabel:ConvertLabel','GetClasses=GetClasses:GetClasses']},
     py_modules=['DataML','ConvertLabel','GetClasses','libDataML'],
-    version='20221103a',
+    version='20230217a',
     description='Multilabel machine learning for combined experimental data',
     long_description= """ Multilabel machine learning for combined experimental data """,
     author='Nicola Ferralis',
