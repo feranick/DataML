fe3ab2e58566d2013fea5c4bf6bd4b83200a221f
Nicola Ferralis <feranick@hotmail.com>
2025-03-07 19:30:51 -0500

    plot initial plots

--- src/AddDenoiseAutoEncoder.py
+++ src/AddDenoiseAutoEncoder.py
@@ -135,7 +135,7 @@ def main():
         newA = A
         norm = 0
     
-    #plotAugmData(A.shape, A, rootFile+"_initial-plots.pdf")
+    plotAugmData(A.shape, A, rootFile+"_initial-plots.pdf")
     
     success = 0
     for i in range(dP.numAdditions):
