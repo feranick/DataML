4fbda6e5af39ab3daa23f845bdf9db2d815878cf
Nicola Ferralis <feranick@hotmail.com>
2025-03-27 16:54:04 -0400

    Bump to version 2025.3.27.1

--- src/DataML_DF.py
+++ src/DataML_DF.py
@@ -3,7 +3,7 @@
 '''
 *****************************************************
 * DataML Decision Forests - Classifier and Regressor
-* version: 2025.03.25.1
+* version: 2025.03.27.1
 * Uses: sklearn
 * By: Nicola Ferralis <feranick@hotmail.com>
 *****************************************************
