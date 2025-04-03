Additional fixes to Normalization
Do not overwrite initial normalization with validation data during training.

src/DataML.py
@@ -3,7 +3,7 @@
 '''
 ***********************************************
 * DataML Classifier and Regressor
-* version: 2025.04.03.1
+* version: 2025.04.03.2
 * Uses: Keras, TensorFlow
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************
@@ -282,9 +282,9 @@ def train(learnFile, testFile, normFile):
     
     learnFileRoot = os.path.splitext(learnFile)[0]
 
-    En, A, Cl, _ = readLearnFile(learnFile, dP)
+    En, A, Cl, _ = readLearnFile(learnFile, True, dP)
     if testFile is not None:
-        En_test, A_test, Cl_test, _ = readLearnFile(testFile, dP)
+        En_test, A_test, Cl_test, _ = readLearnFile(testFile, False, dP)
         totA = np.vstack((A, A_test))
         totCl = np.append(Cl, Cl_test)
     else:
@@ -791,7 +791,7 @@ def batchPredict(folder, normFile):
 #************************************************************
 def validBatchPredict(testFile, normFile):
     dP = Conf()
-    En_test, A_test, Cl_test, _ = readLearnFile(testFile, dP)
+    En_test, A_test, Cl_test, _ = readLearnFile(testFile, False, dP)
     model = loadModel(dP)
 
     if normFile is not None: