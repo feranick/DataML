b85bfb73474728ef8abcfeddeb9a8f8724516e69
Nicola Ferralis <feranick@hotmail.com>
2025-06-04 09:48:42 -0400

    Use current folder in URL to name seleIndex cookie
    No need to edit igc_ml.js

--- web/DataML_DF/pyscript/igc_ml.js
+++ web/DataML_DF/pyscript/igc_ml.js
@@ -1,5 +1,5 @@
 var modelPkl = 0;
-var nameSelIndex = "selectedIndex_custom"
+var nameSelIndex="selIndex_"+window.location.pathname.split('/').slice(-2)[0];
 
 function showLog() {
   const url="./";
