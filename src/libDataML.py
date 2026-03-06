d80fdc165e343a8b895e3163564ae2540ac3e4fb
Nicola Ferralis <feranick@hotmail.com>
2026-03-06 17:23:57 -0500

    Bump version

--- src/libDataML.py
+++ src/libDataML.py
@@ -2,7 +2,7 @@
 '''
 **************************************************
 * libDataML - Library for DataML/DataML_DF
-* version: 2026.03.06.2
+* version: 2026.03.06.3
 * Uses: Keras, TensorFlow, scikit-learn
 * By: Nicola Ferralis <feranick@hotmail.com>
 **************************************************
