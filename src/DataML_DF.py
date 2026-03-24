Fix Normalization druing prediction in DF

src/DataML_DF.py
@@ -203,15 +203,15 @@ def main():
 
     for o, a in opts:
         if o in ("-t" , "--train"):
-            try:
-                dP.updateConfig('Parameters','featureReduction','False')
-                if len(sys.argv) == 3:
-                    train(sys.argv[2], None)
-                else:
-                    train(sys.argv[2], sys.argv[3])
-            except Exception as e:
-                print(f" An error occurred: {e}\n")
-                sys.exit(2)
+            #try:
+            dP.updateConfig('Parameters','featureReduction','False')
+            if len(sys.argv) == 3:
+                train(sys.argv[2], None)
+            else:
+                train(sys.argv[2], sys.argv[3])
+            #except Exception as e:
+            #    print(f" An error occurred: {e}\n")
+            #    sys.exit(2)
             
         if o in ("-r" , "--reduce"):
             try:
@@ -440,16 +440,16 @@ def train(learnFile, testFile):
 
     if dP.regressor:
         if dP.normalize:
-            pred = norm.transform_inverse(df.predict(A_test))
-            Cl_test = norm.transform_inverse(Cl_test)
+            pred = norm.transform_inverse_multiple(df.predict(A_test))
+            Cl_test = norm.transform_inverse_multiple(Cl_test)
         else:
             pred = df.predict(A_test)
         score = mean_absolute_error(pred, Cl_test)
     else:
         if dP.normalize:
-            pred = norm.transform_inverse(np.asarray(le.inverse_transform_bulk(df.predict(A_test))))
-            pred_classes = norm.transform_inverse(np.asarray(le.inverse_transform_bulk(df.classes_)))
-            Cl_test = norm.transform_inverse(Cl_test)
+            pred = norm.transform_inverse_multiple(np.asarray(le.inverse_transform_bulk(df.predict(A_test))))
+            pred_classes = norm.transform_inverse_multiple(np.asarray(le.inverse_transform_bulk(df.classes_)))
+            Cl_test = norm.transform_inverse_multiple(Cl_test)
         else:
             pred = le.inverse_transform_bulk(df.predict(A_test))
             pred_classes = le.inverse_transform_bulk(df.classes_)

src/libDataML.py
@@ -97,8 +97,8 @@ class Normalizer(object):
         return vn
 
     # Multiple samples, format as vertical stacks of singles
-    def transform_inverse(self,V):
-        return self.min + V * (self.max - self.min) / self.YnormTo
+    def transform_inverse_multiple(self,V):
+        return self.min[0] + V * (self.max[0] - self.min[0]) / self.YnormTo
 
     # format simular to original DataML file
     def transform_inverse_features(self,V):
@@ -107,6 +107,10 @@ class Normalizer(object):
             vn = (self.min[i] + + V[1:,i]*(self.max[i] - self.min[i])/self.YnormTo)
             Vn.append(vn)
         return np.vstack([V[0,:],np.vstack(Vn).T])
+        
+    # Column by column, format as vertical stacks of singles
+    def transform_inverse(self,V):
+        return self.min + V * (self.max - self.min) / self.YnormTo
 
     def save(self):
         with open(self.norm_file, 'wb') as f: