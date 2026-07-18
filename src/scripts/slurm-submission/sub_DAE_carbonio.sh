b6d024d47d9bdac581462c3115b74a6a5f49f2e9
Nicola Ferralis <feranick@hotmail.com>
2026-07-17 21:08:03 -0400

    Added new submission scripts for slurm
    They are mult-job-per-node

---
+++ src/scripts/slurm-submission/sub_DAE_carbonio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=1                  # Request 2 nodes
+#SBATCH -t 100:00:00                # Request 12 hours in walltime.
+#SBATCH -J DAE_P6_M1           # Job name
+#SBATCH -o log_%x.o%j              # output file
+#SBATCH -e log_%x.e%j              # error file
+#SBATCH --partition=long
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2          # raise only if your code is multithreaded
+#SBATCH --gres=mps:8              # ~8% of the GPU; ~12 jobs share it
+#SBATCH --mem=2500M 
+#SBATCH --export=ALL
+
+#command="srun train_rruff_raman.sh $SLURM_JOB_NAME "
+#command="srun train_rruff_raman.sh $1"
+#command="srun ../train_rruff_ir.sh $(basename $(pwd))"
+
+command="DataML_DAE -a $1"
+
+echo $command
+
+$command
